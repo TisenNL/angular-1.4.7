3712d54ca3d5a9dbf9d16cb1ef3745615d40e4e8
TIAGO NEVES LEAO COSTA LINO <tiagobrasil18@gmail.com>
2023-10-28 10:16:04 -0300

    lesson 13

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,37 +1,15 @@
-// <reference path="angular.min.js" />
+var app = angular
+  .module("myModule", [])
+  .controller("myController", function ($scope) {
 
-// Create an Angular module named "myModule" with an empty dependency []
-var myApp = angular
-   .module("myModule", [])
-   // Define a controller named "myController" and inject $scope into it
-   .controller("myController", function ($scope) {
+    var employees = [
+      { name: "Ben", gender: 1, salary: 55000 },
+      { name: "Sara", gender: 2, salary: 68000 },
+      { name: "Mark", gender: 1, salary: 57000 },
+      { name: "Pam", gender: 2, salary: 53000 },
+      { name: "Todd", gender: 3, salary: 60000 }
+    ];
 
-     // Define an array of employee objects
-     var employees = [
-      { name: "Ben", gender: "Male", salary: 55000, city: "London" },
-      { name: "Sara", gender: "Female", salary: 68000, city: "Chennai" },
-      { name: "Mark", gender: "Female", salary: 57000, city: "London" },
-      { name: "Pam", gender: "Female", salary: 53000, city: "Chennai" },
-      { name: "Todd", gender: "Male", salary: 60000, city: "London" }
-     ];
-
-     // Associate the array of employees with $scope to make it accessible in the HTML
-     $scope.employees = employees;
-
-     // Define a search function that checks if the item matches the search text
-     $scope.search = function (item) {
-        // Check if searchText is undefined and if so, return true (no filtering)
-        if($scope.searchText == undefined) {
-          return true;
-        } else {
-          // Check if the item's name or city matches searchText (case-insensitive)
-          if(item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 || 
-            item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
-            return true; // If there's a match, return true (the item should be displayed)
-          }
-        }
-
-        return false; // If there's no match, return false (the item should not be displayed)
-     }
-   });
+    $scope.employees = employees;
+  });
 
